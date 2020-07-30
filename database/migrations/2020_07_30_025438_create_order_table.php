4e4e9f4f53ac1fd16f8722a93123e7dc702378c6
AIndraFata <ahmad_indra_28rpl@student.smktelkom-mlg.sch.id>
2020-07-30 09:59:40 +0700

    "Migrasi database db_toko

---
+++ database/migrations/2020_07_30_025438_create_order_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->bigIncrements('id_order');
+            $table->unsignedBigInteger('id_customers');
+            $table->unsignedBigInteger('id_product');
+
+            $table->foreign('id_customers')->references('id_customers')->on('customers');
+            $table->foreign('id_product')->references('id_product')->on('product');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
