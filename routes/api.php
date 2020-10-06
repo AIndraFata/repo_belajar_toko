2cb14d244442dacd5c85cf733ab3fd8d31973936
AIndraFata <ahmad_indra_28rpl@student.smktelkom-mlg.sch.id>
2020-10-06 21:07:08 +0700

    Menambah pembagian hak akses

--- routes/api.php
+++ routes/api.php
@@ -7,21 +7,36 @@ Route::post('/login', 'UserController@login');
 
 Route::group(['middleware' => ['jwt.verify']], function ()
 {
+    Route::group(['middleware' => ['api.superadmin']], function()
+    {
+        Route::delete('/customers/{id}', 'customersController@destroy');
+        Route::delete('/product/{id}', 'productController@destroy');
+        Route::delete('/order/{id}', 'orderController@destroy');
+    });
+
+    Route::group(['middleware' => ['api.admin']], function () 
+    {
+        Route::post('/customers', 'customersController@store');
+        Route::put('/customers/{id}', 'customersController@update');
+
+        Route::post('/product', 'productController@store');
+        Route::put('/product/{id}', 'productController@update');
+
+        Route::post('/order', 'orderController@store');
+        Route::put('/order/{id}', 'orderController@update');
+    });
+
     Route::get('/customers', 'customersController@show');
     Route::post('/customers', 'customersController@store');
-    Route::put('/customers/{id}', 'customersController@update');
-    Route::delete('/customers/{id}', 'customersController@destroy');
-
+    
+    
     Route::get('/product', 'productController@show');
-    Route::post('/product', 'productController@store');
-    Route::put('/product/{id}', 'productController@update');
-    Route::delete('/product/{id}', 'productController@destroy');
+    
 
     Route::get('/order', 'orderController@show');
     Route::get('/order/{id}', 'orderController@detail');
-    Route::post('/order', 'orderController@store');
-    Route::put('/order/{id}', 'orderController@update');
-    Route::delete('/order/{id}', 'orderController@destroy');
+    
+    
 });
 
 /*
